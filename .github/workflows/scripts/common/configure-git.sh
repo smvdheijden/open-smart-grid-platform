70e82bed057db094df9d2f3e7ca7fe8dddaf70a9
Sander van der Heijden <sander.van.der.heijden@alliander.com>
2026-04-02 14:41:54 +0200

    Adds printenv for testing

--- .github/workflows/scripts/common/configure-git.sh
+++ .github/workflows/scripts/common/configure-git.sh
@@ -8,7 +8,7 @@ echo "::debug:: ENV_FILE: $ENV_FILE"
 # shellcheck source=../../.env
 source "$ENV_FILE"
 
-echo "TOKEN: $TOKEN"
+printenv | sort
 
 git config --global user.name "$GIT_USER"
 git config --global user.email "$GIT_EMAIL"
